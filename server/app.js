1aa0ecb80668baf82eaaf63c2b7090319e4965c8
hackerrank <me@hackerrank.com>
2021-02-19 14:02:34 +0700

    deploy heroku

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const app = require('express')();
 app.get('/', (req, res) => {
   res.send('Whack a mole project')
 })
+const PORT = process.env.PORT || 3000;
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
   cors: {
@@ -86,6 +87,6 @@ io.on('connection', (socket) => {
   })
 })
 
-http.listen(3000, () => {
+http.listen(PORT, () => {
   console.log('Server started!');
 })
